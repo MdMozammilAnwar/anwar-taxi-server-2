756a5a6a3cf57bb109cc0336d85bf676933921a8
Anwar <anwar@uexcelerate.com>
2023-10-21 23:07:51 +0530

    Dialect added explicitly

--- configs/db.js
+++ configs/db.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 const dbConfig = {
-  connectionString: process.env.POSTGRES_URL + "?sslmode=require"
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  dialect: process.env.DB_DIALECT
 };
 
 let config = dbConfig;
