968be49b8c7fb821d7d1e479454a68ac55c613fd
Anwar <anwar@uexcelerate.com>
2023-10-21 23:52:04 +0530

    Dialect added explicitly

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.json({ limit: "50mb", extended: true }));
 
 var whitelist = [
+  "https://anwar-taxi-web-app.vercel.app/",
   "http://localhost:4200",
   "http://localhost:4201"
 ];
